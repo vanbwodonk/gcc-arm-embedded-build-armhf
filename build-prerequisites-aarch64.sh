36604fc8b95dbdfbd50d5c0459e1c48533d3594c
Arif Darmawan <arif.pens@gmail.com>
2019-09-11 10:12:01 +0700

    edit build script for raspberry pi

--- build-prerequisites-aarch64.sh
+++ build-prerequisites-aarch64.sh
@@ -35,11 +35,6 @@ umask 022
 
 exec < /dev/null
 
-# Added for Raspberry Pi
-LIBRARY_PATH=/usr/lib/$(gcc -print-multiarch)
-CPATH=/usr/include/$(gcc -print-multiarch)
-export LIBRARY_PATH CPATH
-
 script_path=`cd $(dirname $0) && pwd -P`
 . $script_path/build-common.sh
 
@@ -122,7 +117,8 @@ echo Task [I-1] /$HOST_NATIVE/gmp/
 rm -rf $BUILDDIR_NATIVE/gmp && mkdir -p $BUILDDIR_NATIVE/gmp
 pushd $BUILDDIR_NATIVE/gmp
 
-CPPFLAGS="-fexceptions" $SRCDIR/$GMP/configure \
+CPPFLAGS="-fexceptions" $SRCDIR/$GMP/configure --build=$BUILD \
+    --host=$HOST_NATIVE \
     --prefix=$BUILDDIR_NATIVE/host-libs/usr \
     --enable-cxx \
     --disable-shared \
@@ -137,7 +133,8 @@ echo Task [I-2] /$HOST_NATIVE/mpfr/
 rm -rf $BUILDDIR_NATIVE/mpfr && mkdir -p $BUILDDIR_NATIVE/mpfr
 pushd $BUILDDIR_NATIVE/mpfr
 
-$SRCDIR/$MPFR/configure \
+$SRCDIR/$MPFR/configure --build=$BUILD \
+    --host=$HOST_NATIVE \
     --target=$TARGET \
     --prefix=$BUILDDIR_NATIVE/host-libs/usr \
     --disable-shared \
@@ -153,7 +150,8 @@ echo Task [I-3] /$HOST_NATIVE/mpc/
 rm -rf $BUILDDIR_NATIVE/mpc && mkdir -p $BUILDDIR_NATIVE/mpc
 pushd $BUILDDIR_NATIVE/mpc
 
-$SRCDIR/$MPC/configure \
+$SRCDIR/$MPC/configure --build=$BUILD \
+    --host=$HOST_NATIVE \
     --target=$TARGET \
     --prefix=$BUILDDIR_NATIVE/host-libs/usr \
     --disable-shared \
@@ -170,7 +168,8 @@ echo Task [I-4] /$HOST_NATIVE/isl/
 rm -rf $BUILDDIR_NATIVE/isl && mkdir -p $BUILDDIR_NATIVE/isl
 pushd $BUILDDIR_NATIVE/isl
 
-$SRCDIR/$ISL/configure \
+$SRCDIR/$ISL/configure --build=$BUILD \
+    --host=$HOST_NATIVE \
     --target=$TARGET \
     --prefix=$BUILDDIR_NATIVE/host-libs/usr \
     --disable-shared \
@@ -186,7 +185,8 @@ echo Task [I-5] /$HOST_NATIVE/libelf/
 rm -rf $BUILDDIR_NATIVE/libelf && mkdir -p $BUILDDIR_NATIVE/libelf
 pushd $BUILDDIR_NATIVE/libelf
 
-$SRCDIR/$LIBELF/configure \
+$SRCDIR/$LIBELF/configure --build=$BUILD \
+    --host=$HOST_NATIVE \
     --target=$TARGET \
     --prefix=$BUILDDIR_NATIVE/host-libs/usr \
     --disable-shared \
@@ -201,7 +201,8 @@ echo Task [I-6] /$HOST_NATIVE/expat/
 rm -rf $BUILDDIR_NATIVE/expat && mkdir -p $BUILDDIR_NATIVE/expat
 pushd $BUILDDIR_NATIVE/expat
 
-$SRCDIR/$EXPAT/configure \
+$SRCDIR/$EXPAT/configure --build=$BUILD \
+    --host=$HOST_NATIVE \
     --target=$TARGET \
     --prefix=$BUILDDIR_NATIVE/host-libs/usr \
     --disable-shared \
@@ -210,9 +211,6 @@ $SRCDIR/$EXPAT/configure \
 make -j$JOBS
 make install
 popd
-
-# for Raspberry Pi, always skip mingw32
-exit 0
 
 # skip building mingw32 toolchain if "--skip_mingw32" specified
 if [ "x$skip_mingw32" == "xyes" ] ; then
